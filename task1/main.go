9afbd09ba024434ec03062d8c8ec2a9cd2ef6640
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-12 19:44:06 +0300

    feat: Add user input parsing

--- task1/main.go
+++ task1/main.go
@@ -2,17 +2,74 @@ package main
 
 import (
 	"fmt"
-	// "bufio"
-	// "os"
+	"bufio"
+	"os"
 	"strings"
 	"strconv"
+	"errors"
 )
 
 func main() {
-	// reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter your Full Name: ")
+	fullName, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Invalid input")
+		return 
+	}
+
+	fullName = strings.Trim(fullName, "\n")
+
+	fmt.Print("Enter number of courses you have taken: ")
+	
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Invalid input")
+		return 
+	}
+
+	courseCount, err := strconv.ParseInt(strings.Trim(input, "\n"), 10, 64)
+	if err != nil {
+		fmt.Println("Invalid number of courses. Please try again.")
+		return
+	}
+
+	fmt.Print("Enter course names: ")
+	names, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+
+	courseNames := strings.Fields(names)
+	if int64(len(courseNames)) != courseCount {
+		fmt.Printf("Expected %d courses, found %d courses", courseCount, len(courseNames))
+		return
+	}
+
+	fmt.Print("Enter course grades: ")
+	grades, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Invalid input")
+		return 
+	}
+
+	courseGrades, err := parseGrades(grades)
+	if err != nil {
+		fmt.Println("Invalid grades")
+		return
+	}
 
 	
-	fmt.Println("Hello World!")
+	if int64(len(courseGrades)) != courseCount {
+		fmt.Printf("Expected %d courses, found %d courses", courseCount, len(courseGrades))
+		return
+	}
+	
+
+	
+	fmt.Printf("Full Name: %s\nNumber of Courses: %d\n", fullName, courseCount)
 }
 
 func parseGrades(grades string) ([]float64, error) {
@@ -22,6 +79,10 @@ func parseGrades(grades string) ([]float64, error) {
 		grade, err := strconv.ParseFloat(field, 64)
 		if err != nil {
 			return nil, err 
+		}
+
+		if grade < 0 || grade > 100 {
+			return nil, errors.New("Invalid grade value") 
 		}
 
 		res = append(res, grade)
