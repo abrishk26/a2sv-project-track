dc49b92a059a8cf990d6946a2298057907aa2b43
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-12 18:59:44 +0300

    feat: add function to parse grades

--- task1/main.go
+++ task1/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	// "bufio"
+	// "os"
+	"strings"
+	"strconv"
 )
 
 func main() {
+	// reader := bufio.NewReader(os.Stdin)
+
+	
 	fmt.Println("Hello World!")
+}
+
+func parseGrades(grades string) ([]float64, error) {
+	res := []float64{}
+
+	for _, field := range strings.Fields(grades) {
+		grade, err := strconv.ParseFloat(field, 64)
+		if err != nil {
+			return nil, err 
+		}
+
+		res = append(res, grade)
+	}
+
+	return res, nil
 }
