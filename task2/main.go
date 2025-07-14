d2dbb7751dc1883ed42a7274fcc332d1e7735a6c
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-14 11:34:16 +0300

    feat: Add a function to count word frequencies in a string

--- task2/main.go
+++ task2/main.go
@@ -1,6 +1,26 @@
-package main
+package task2
+
+import (
+	"unicode"
+	"strings"
+)
 
 
-func main() {
+func CountWordFreq(input string) map[string]int  {
+	count := map[string]int{}
+	fields := strings.Fields(input)
+
+	for _, field := range fields {
+		word := []rune{}
+
+		for _, r := range field {
+			if unicode.IsLetter(r) {
+				word = append(word, unicode.ToLower(r))
+			}
+		}
+
+		count[string(word)]++
+	}
 	
+	return count
 }
