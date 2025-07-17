c5e60c7d8cfaae332d6870d023abb7b1ee5b16d0
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-17 17:02:19 +0300

    feat: setup .env file loading

---
+++ task5/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s", err.Error())
+		return
+	}
+}
