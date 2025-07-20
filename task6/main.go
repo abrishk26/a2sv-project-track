f2a86257a747d85346f2c28e4502ba32df103dc9
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-20 17:12:56 +0300

    feat: load env variales, setup server and db connection

---
+++ task6/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/abrishk26/a2sv-project-track/task6/router"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+		return
+	}
+
+	clientOption := options.Client().ApplyURI(os.Getenv("mongo"))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client, err := mongo.Connect(clientOption)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+		return
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+		return
+	}
+
+	fmt.Println("Database connected sucessfully")
+
+	taskCollection := client.Database("test").Collection("tasks")
+	userCollection := client.Database("test").Collection("users")
+
+	router := router.NewRouter(taskCollection, userCollection)
+
+	router.Run()
+}
