74889f7ae3070d0d6bb19d548f7f5db07fad573a
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-24 13:26:29 +0300

    feat: add functions to generate and validate jwt token

---
+++ task7/Infrastructure/jwt_service.go
@@ -0,0 +1,38 @@
+package infrastructures
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(claims map[string]any, secret string) (string, error) {
+	c := jwt.MapClaims{}
+	for claim, value := range claims {
+		c[claim] = value
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
+
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyToken(token, secret string, claims jwt.Claims) (*jwt.Token, error) {
+	parsedToken, err := jwt.ParseWithClaims(token, &claims, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return parsedToken, nil
+}
